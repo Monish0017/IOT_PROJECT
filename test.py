02069567ce51a54ef0e01eff36aca210b3a17831
Monish0017 <monishrajan0017@gmail.com>
2025-02-28 21:50:12 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import keyboard
+
+app = Flask(__name__)
+CORS(app)
+
+latest_gesture = ""  # Store last received gesture
+
+@app.route('/')
+def home():
+    return "ESP8266 Data Receiver Running!"
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    global latest_gesture
+    latest_gesture = request.data.decode("utf-8").strip()
+    print(f"Received Gesture: {latest_gesture}")
+    return "Data Received", 200
+
+@app.route('/get_latest_gesture', methods=['GET'])
+def get_latest_gesture():
+    return latest_gesture  # Return latest received gesture
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
